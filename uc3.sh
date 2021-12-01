a20793d25ae85f709f103cac292f1ebcb6cabd02
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2021-12-01 16:49:14 +0530

    added usecase3

---
+++ uc3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+#Variables
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if (( $isFullTime==$randomCheck ))
+then
+	empHrs=8;
+elif (( $isPartTime==$randomCheck ))
+then
+	empHrs=4;
+else
+	empHrs=0;
+fi
+salary=$(($empHrs*$empRatePerHr));
+
